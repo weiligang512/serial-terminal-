f03eb0301bc404708e67b51f3fe59b42a5fc70a9
weiligang512 <weiligang512@gmail.com>
2012-06-02 23:15:04 +0800

    add communication

--- inc/comm.h
+++ inc/comm.h
@@ -12,9 +12,19 @@
 #ifndef __COMM_H__
 #define __COMM_H__
 
+#define HEAD1	0x55
+#define HEAD2	0xaa
 
 
 
+/* **********************CMD**************/
+#define OTHER			0x00			//其他 
+#define SETSYSPARA		0x01		    //设置系统参数
+#define SETTIME			0x02			//设置时间
+#define READTIME		0x03			//读时间
+#define READTH			0x04			//读温度湿度
+#define IRDASEND		0x05			//红外发码 
+/* ***************************************/
 
 
 
